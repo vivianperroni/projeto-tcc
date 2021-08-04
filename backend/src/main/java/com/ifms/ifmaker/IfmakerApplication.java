d3dfae46110d3db0796972aecc6644419d07cce4
vivianperroni <vivian.perroni@hotmail.com>
2021-08-03 21:20:55 -0400

    Projetos criados

---
+++ backend/src/main/java/com/ifms/ifmaker/IfmakerApplication.java
@@ -0,0 +1,13 @@
+package com.ifms.ifmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmakerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ifms/ifmaker/IfmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifms.ifmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
