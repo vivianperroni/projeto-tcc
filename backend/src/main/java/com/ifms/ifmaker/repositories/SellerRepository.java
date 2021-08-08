2a7ad5dc66705a50201feefd9221b302b7ccad0a
vivianperroni <vivian.perroni@hotmail.com>
2021-08-08 16:34:10 -0400

    Camadas

---
+++ backend/src/main/java/com/ifms/ifmaker/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.ifms.ifmaker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ifms.ifmaker.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/ifms/ifmaker/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.ifms.ifmaker.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifms.ifmaker.dto.SellerDTO;
+import com.ifms.ifmaker.entities.Seller;
+import com.ifms.ifmaker.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
