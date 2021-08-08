18699263086e25dd3a3722993d0fb2b8900e2d11
vivianperroni <vivian.perroni@hotmail.com>
2021-08-08 18:27:43 -0400

    Agrupar por Pesquisa

--- backend/src/main/java/com/ifms/ifmaker/service/SaleService.java
+++ backend/src/main/java/com/ifms/ifmaker/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.ifms.ifmaker.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ifms.ifmaker.dto.SaleDTO;
+import com.ifms.ifmaker.dto.SaleSuccessDTO;
+import com.ifms.ifmaker.dto.SaleSumDTO;
 import com.ifms.ifmaker.entities.Sale;
 import com.ifms.ifmaker.repositories.SaleRepository;
 import com.ifms.ifmaker.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
