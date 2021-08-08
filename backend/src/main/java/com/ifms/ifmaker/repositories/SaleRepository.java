1e120d9ed1df5990fb9c0a0ff38aceb6f7ca158a
vivianperroni <vivian.perroni@hotmail.com>
2021-08-08 17:26:58 -0400

    Paginação

---
+++ backend/src/main/java/com/ifms/ifmaker/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.ifms.ifmaker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ifms.ifmaker.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/ifms/ifmaker/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.ifms.ifmaker.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ifms.ifmaker.dto.SaleDTO;
+import com.ifms.ifmaker.entities.Sale;
+import com.ifms.ifmaker.repositories.SaleRepository;
+import com.ifms.ifmaker.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
