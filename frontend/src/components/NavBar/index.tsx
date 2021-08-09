820ce4a71ff3a99706811685be32ce766c2fd830
vivianperroni <vivian.perroni@hotmail.com>
2021-08-09 17:36:27 -0400

    Integração DataTable

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import ImgDsDark from 'assets/img/ds-dark.svg';
+import ImgIfmaker from 'assets/img/ifmaker.jpg';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
@@ -7,7 +7,7 @@ const NavBar = () => {
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
                     <Link to="/">
-                        <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                        <img src={ImgIfmaker} alt="DevSuperior" width="120" />
                     </Link>
                 </nav>
             </div>
